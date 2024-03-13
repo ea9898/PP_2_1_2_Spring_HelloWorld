6f10c43950347dc59cb91de7f44588f0c70897b2
sevgeniy <ea9898@yandex.ru>
2024-03-13 09:46:00 +0300

    PP_2_1_2_Spring_HelloWorld решение

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,10 +3,17 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class App {
     public static void main(String[] args) {
-        ApplicationContext applicationContext =
-                new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld helloWorld1 = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld helloWorld2 = (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
+
+        System.out.println(helloWorld1.getMessage());
+        System.out.println(cat1.getName());
+
+        System.out.println(helloWorld1.equals(helloWorld2));
+        System.out.println(cat1.equals(cat2));
     }
 }
